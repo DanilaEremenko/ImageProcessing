f4c3c0b853fa82237e4844bd6b5f41bc082d4cea
Danila <danilaeremenko@mail.ru>
2020-11-26 20:04:43 +0300

    ImageProcessing: bunch of refactoring between full_conv(...) and convolve_and_show(...)

--- lab2/conv_filters.py
+++ lab2/conv_filters.py
@@ -8,34 +8,38 @@ def conv(img_sub_arr, kernel):
 
 
 def full_conv(img_arr, kernel):
+    # expand
+    if len(kernel) > 2:
+        expanded_arr = img_arr
+
+        expanded_arr = np.concatenate((expanded_arr[0][np.newaxis], img_arr), axis=0)
+        expanded_arr = np.concatenate((expanded_arr[:, 0][:, np.newaxis], expanded_arr), axis=1)
+        expanded_arr = np.concatenate((expanded_arr, expanded_arr[-1][np.newaxis]), axis=0)
+        expanded_arr = np.concatenate((expanded_arr, expanded_arr[:, -1][:, np.newaxis]), axis=1)
+
+        expanded_arr = np.array(expanded_arr, dtype='uint8')
+    else:
+        expanded_arr = img_arr
+
+    # convolve
     res = np.zeros(img_arr.shape)
-    for x in range(0, img_arr.shape[0] - kernel.shape[0] + 1):
-        for y in range(0, img_arr.shape[1] - kernel.shape[1] + 1):
+    for x in range(0, expanded_arr.shape[0] - kernel.shape[0] + 1):
+        for y in range(0, expanded_arr.shape[1] - kernel.shape[1] + 1):
             res[x][y] = conv(
-                img_sub_arr=img_arr[x:x + kernel.shape[0], y:y + kernel.shape[1]],
+                img_sub_arr=expanded_arr[x:x + kernel.shape[0], y:y + kernel.shape[1]],
                 kernel=kernel
             )
+
+    # cut if was expanded
+    if expanded_arr.shape != img_arr.shape:
+        res = res[1:-1, 1:-1]
+
     return res
 
 
 def convolve_and_show(img_arr, kernel=None, title=None):
     if kernel is not None:
-        if len(kernel) > 2:
-            expanded_arr = img_arr.copy()
-
-            expanded_arr = np.concatenate((expanded_arr[0][np.newaxis], expanded_arr), axis=0)
-            expanded_arr = np.concatenate((expanded_arr[:, 0][:, np.newaxis], expanded_arr), axis=1)
-            expanded_arr = np.concatenate((expanded_arr, expanded_arr[-1][np.newaxis]), axis=0)
-            expanded_arr = np.concatenate((expanded_arr, expanded_arr[:, -1][:, np.newaxis]), axis=1)
-
-            expanded_arr = np.array(expanded_arr, dtype='uint8')
-
-            res = full_conv(expanded_arr, kernel)
-            res = res[1:-1, 1:-1]  # remove expanded part
-        else:
-            res = full_conv(img_arr, kernel)
-
-        res = np.array(res)
+        res = full_conv(img_arr, kernel)
     else:
         res = img_arr
 
