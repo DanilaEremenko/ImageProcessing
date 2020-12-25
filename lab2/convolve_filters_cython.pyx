dc23b779f10c0dc764c628db9af7cbc6c227b779
Danila <danilaeremenko@mail.ru>
2020-12-25 22:21:07 +0300

    ImageProcessing: lab2/convolve_filters_cython.pyx updated

--- lab2/convolve_filters_cython.pyx
+++ lab2/convolve_filters_cython.pyx
@@ -1,6 +1,6 @@
 def conv_cython(img_sub_arr, kernel):
-    cdef int kernel_len = len(img_sub_arr)
-    cdef float sum = 0
+    kernel_len = len(img_sub_arr)
+    sum = 0
     for x in range(kernel_len):
         for y in range(kernel_len):
             sum += img_sub_arr[x, y] * kernel[x, y]
