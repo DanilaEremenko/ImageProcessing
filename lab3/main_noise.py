0cdd46a9ae0ccd7002769e126f5e5e93e117837b
Danila <danilaeremenko@mail.ru>
2021-02-02 19:07:25 +0300

    ImageProcessing: lab3/main_noise.py refactored

--- lab3/main_noise.py
+++ lab3/main_noise.py
@@ -2,20 +2,26 @@ import json
 
 import cv2
 import scipy.optimize as optimizers
-from convolve_filters import draw_image, calculate_diff, full_conv, full_adaptive_conv
+from convolve_filters import calculate_diff, full_conv, full_adaptive_conv
 from noise_filters import get_gaussian_kernel_py, get_bilateral_kernel_np, non_local_means, get_bilateral_kernel_py
 import numpy as np
 from PIL import Image
 import time
 import pandas as pd
+from lib.plot_part import show_image
 
 
 ######################################################################
-# -------------------- MAIN ------------------------------------------
+# -------------------- NOISE  ----------------------------------------
 ######################################################################
 
 def get_gaussian_noise(mean, sigma, shape):
     return np.random.normal(mean, sigma, shape)
+
+
+def add_history_step(history, loss, args_json):
+    history['loss'].append(loss)
+    history['args'].append(args_json)
 
 
 ####################################################################################
@@ -35,8 +41,11 @@ def mean_min_func(args, name, orig_img, noised_img, history):
 
     print(f"{len(history['loss']) + 1}.{name}: {diff}")
 
-    history['loss'].append(diff)
-    history['args'].append(json.dumps({'kernel_len': kernel_len}))
+    add_history_step(
+        history=history,
+        loss=diff,
+        args_json=json.dumps({'kernel_len': kernel_len})
+    )
     return diff
 
 
@@ -58,8 +67,11 @@ def gaussian_min_func(args, name, orig_img, noised_img, history):
 
     print(f"{len(history['loss']) + 1}.{name}: {diff}")
 
-    history['loss'].append(diff)
-    history['args'].append(json.dumps({'sigma': sigma, 'kernel_len': kernel_len}))
+    add_history_step(
+        history=history,
+        loss=diff,
+        args_json=json.dumps({'sigma': sigma, 'kernel_len': kernel_len})
+    )
     return diff
 
 
@@ -86,8 +98,11 @@ def bilateral_min_func(args, name, orig_img, noised_img, history):
 
     print(f"{len(history['loss']) + 1}.{name}: {diff}")
 
-    history['loss'].append(diff)
-    history['args'].append(json.dumps({'sigma_i': sigma_i, 'sigma_s': sigma_s, 'kernel_len': kernel_len}))
+    add_history_step(
+        history=history,
+        loss=diff,
+        args_json=json.dumps({'sigma_i': sigma_i, 'sigma_s': sigma_s, 'kernel_len': kernel_len})
+    )
     return diff
 
 
@@ -111,31 +126,35 @@ def nlm_min_func(args, name, orig_img, noised_img, history):
 
     print(f"{len(history['loss']) + 1}.{name}: {diff}")
 
-    history['loss'].append(diff)
-    history['args'].append(json.dumps({'sigma': sigma, 'h': h}))
+    add_history_step(
+        history=history,
+        loss=diff,
+        args_json=json.dumps({'sigma': sigma, 'h': h})
+    )
     return diff
 
 
 ####################################################################################
-def get_img_from_name(name, args):
+def get_img_from_name(name, orig_img, noised_img, args):
     if name == 'MEAN FILTER':
         kernel_len = args['kernel_len']
-        res_img, _ = get_mean_results(kernel_len)
+        res_img, _ = get_mean_results(orig_img=orig_img, noised_img=noised_img, kernel_len=kernel_len)
 
     elif name == 'GAUSSIAN FILTER':
         sigma = args['sigma']
         kernel_len = args['kernel_len']
-        res_img, _ = get_gaussian_results(sigma=sigma, kernel_len=kernel_len)
+        res_img, _ = get_gaussian_results(orig_img=orig_img, noised_img=noised_img, kernel_len=kernel_len, sigma=sigma)
 
     elif name == 'PYTHON BILATERAL FILTER':
         sigma_i = args['sigma_i']
         sigma_s = args['sigma_s']
         kernel_len = args['kernel_len']
-        res_img, _ = get_bilateral_results(sigma_i=sigma_i, sigma_s=sigma_s, kernel_len=kernel_len)
+        res_img, _ = get_bilateral_results(orig_img=orig_img, noised_img=noised_img, sigma_i=sigma_i, sigma_s=sigma_s,
+                                           kernel_len=kernel_len)
     elif name == 'Non-local means':
         h = args['h']
         sigma = args['sigma']
-        res_img, diff = get_nlm_results(sigma=sigma, h=h)
+        res_img, _ = get_nlm_results(orig_img=orig_img, noised_img=noised_img, sigma=sigma, h=h)
     else:
         raise Exception('Undefined name')
 
@@ -214,13 +233,13 @@ def main():
     orig_img = Image.fromarray(orig_img)
     orig_img.thumbnail((512, 512))
     orig_img = np.asarray(orig_img)
-    draw_image(orig_img, 'ORIGINAL IMAGE')
+    show_image(orig_img, 'ORIGINAL IMAGE')
 
     # noise
     NOISE_SIGMA = 10
 
     noised_img = orig_img + get_gaussian_noise(mean=0, sigma=NOISE_SIGMA, shape=orig_img.shape)
-    draw_image(noised_img, 'NOISED IMAGE')
+    show_image(noised_img, 'NOISED IMAGE')
 
     # run search functions
     res_df = pd.concat([
