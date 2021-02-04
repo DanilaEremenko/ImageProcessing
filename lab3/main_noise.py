6fee887557c40ac5bff69f71aeb5f751e27df85a
Danila <danilaeremenko@mail.ru>
2021-02-04 18:22:43 +0300

    ImageProcessing: lab3 final version

--- lab3/main_noise.py
+++ lab3/main_noise.py
@@ -8,7 +8,7 @@ import numpy as np
 from PIL import Image
 import time
 import pandas as pd
-from lib.plot_part import show_image
+from lib.plot_part import show_image, draw_image_and_loss, draw_images
 
 
 ######################################################################
@@ -142,30 +142,43 @@ def nlm_min_func(args, name, orig_img, noised_img, bw_size, sw_size, h_weight, h
 
 
 ####################################################################################
-def get_img_from_name(name, orig_img, noised_img, args):
+def get_res_from_name(name, orig_img, noised_img, args):
     if name == 'MEAN FILTER':
         kernel_len = args['kernel_len']
-        res_img, _ = get_mean_results(orig_img=orig_img, noised_img=noised_img, kernel_len=kernel_len)
+        res_img, loss = get_mean_results(
+            orig_img=orig_img, noised_img=noised_img,
+            kernel_len=kernel_len
+        )
 
     elif name == 'GAUSSIAN FILTER':
         sigma = args['sigma']
         kernel_len = args['kernel_len']
-        res_img, _ = get_gaussian_results(orig_img=orig_img, noised_img=noised_img, kernel_len=kernel_len, sigma=sigma)
+        res_img, loss = get_gaussian_results(
+            orig_img=orig_img, noised_img=noised_img,
+            kernel_len=kernel_len, sigma=sigma
+        )
 
     elif name == 'PYTHON BILATERAL FILTER':
         sigma_i = args['sigma_i']
         sigma_s = args['sigma_s']
         kernel_len = args['kernel_len']
-        res_img, _ = get_bilateral_results(orig_img=orig_img, noised_img=noised_img, sigma_i=sigma_i, sigma_s=sigma_s,
-                                           kernel_len=kernel_len)
-    elif name == 'Non-local means':
-        h = args['h']
+        res_img, loss = get_bilateral_results(
+            orig_img=orig_img, noised_img=noised_img,
+            sigma_i=sigma_i, sigma_s=sigma_s, kernel_len=kernel_len
+        )
+    elif name == 'NLM':
+        bw_size = args['bw_size']
+        sw_size = args['sw_size']
+        h_weight = args['h_weight']
         sigma = args['sigma']
-        res_img, _ = get_nlm_results(orig_img=orig_img, noised_img=noised_img, sigma=sigma, h=h)
+        res_img, loss = get_nlm_results(
+            orig_img=orig_img, noised_img=noised_img,
+            sigma=sigma, bw_size=bw_size, sw_size=sw_size, h_weight=h_weight
+        )
     else:
         raise Exception('Undefined name')
 
-    return res_img
+    return res_img, loss
 
 
 def get_compare_df(orig_img, noised_img, kernel_len_start, sigma_start, eval_num):
@@ -220,7 +233,7 @@ def get_nlm_df(orig_img, noised_img, sigma_start, eval_num, bw_size, sw_size, h_
         'extra_args': ('NLM-FILTER', orig_img, noised_img, bw_size, sw_size, h_weight),
         'history': {'name': [], 'loss': [], 'args': []}
     }
-    name = 'Non-local means'
+    name = 'NLM'
     optimizers.fmin_bfgs(
         f=hyper_data['func'],
         x0=np.array(list(hyper_data['args'].values())),
@@ -233,16 +246,23 @@ def get_nlm_df(orig_img, noised_img, sigma_start, eval_num, bw_size, sw_size, h_
     return res_df
 
 
-def main(img_path, save_path, noise_sigma):
+def get_original_and_noised(img_path, noise_sigma):
     orig_img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY)
     # orig_img = orig_img[300:450, 300:450]
+
     # original
     orig_img = Image.fromarray(orig_img)
     orig_img.thumbnail((512, 512))
     orig_img = np.asarray(orig_img)
-    show_image(orig_img, 'ORIGINAL IMAGE')
-
+    # noised
     noised_img = orig_img + get_gaussian_noise(mean=0, sigma=noise_sigma, shape=orig_img.shape)
+    return orig_img, noised_img
+
+
+def write_res(img_path, save_path, noise_sigma):
+    orig_img, noised_img = get_original_and_noised(img_path, noise_sigma)
+
+    show_image(orig_img, 'ORIGINAL IMAGE')
     show_image(noised_img, 'NOISED IMAGE')
 
     # run search functions
@@ -277,8 +297,68 @@ def main(img_path, save_path, noise_sigma):
     return res_df
 
 
+def draw_res(image_path, noise_sigma):
+    img_name = image_path.split('/')[-1].split('.')[0]
+    res_name = f'{img_name}_{noise_sigma}'
+    res_path = f'noise_{res_name}.pkl'
+
+    # loading curr_df and selecting columns
+    curr_df = pd.read_pickle(res_path)
+    loss_dict = {
+        name.upper(): {'loss_history': list(curr_df[curr_df['name'] == name]['loss'])}
+        for name in set(curr_df['name']) if name != 'noise'
+    }
+
+    # ordering methods
+    loss_dict = {
+        'MEAN FILTER': loss_dict['MEAN FILTER'],
+        'GAUSSIAN FILTER': loss_dict['GAUSSIAN FILTER'],
+        'PYTHON BILATERAL FILTER': loss_dict['PYTHON BILATERAL FILTER'],
+        'NLM': loss_dict['NLM']
+    }
+    # parse original and noised image
+    orig_img, noised_img = get_original_and_noised(image_path, noise_sigma)
+
+    # getting best results
+    for name in loss_dict.keys():
+        curr_res = curr_df[curr_df['name'] == name]
+        curr_best = curr_res[curr_res['loss'] == curr_res['loss'].min()].iloc[0]
+
+        res_img, actual_loss = get_res_from_name(
+            name=name,
+            orig_img=orig_img,
+            noised_img=noised_img,
+            args=json.loads(str(curr_best['args']))
+        )
+        # assert curr_best['loss'] == actual_loss
+        loss_dict[name]['img'] = res_img
+        loss_dict[name]['best_loss'] = actual_loss
+
+    draw_image_and_loss(
+        loss_keys=list(loss_dict.keys()),
+        loss_list=[res_dict['loss_history'] for res_dict in loss_dict.values()],
+        title=f'{img_name} with sigma = {noise_sigma}',
+        show=True,
+        save_path=f'noise_history_{img_name}_with_sigma_{noise_sigma}.png'
+    )
+
+    draw_images(
+        imgs=[orig_img, noised_img, *[res_dict['img'] for res_dict in loss_dict.values()]],
+        titles=['orig', f'noised with sigma = {noise_sigma}',
+                *[f"{name} with loss = %.2f" % res_dict['best_loss'] for name, res_dict in loss_dict.items()]
+                ],
+        plt_shape=(3, 2),
+        show=True,
+        save_path=f'noise_images_{img_name}_with_sigma_{noise_sigma}.png'
+    )
+
+
 if __name__ == '__main__':
+    draw_results = True
     for image_path in ('../dimages/new_york.webp', '../dimages/test_dog.jpg'):
         for noise_sigma in (10, 15):
-            img_name = image_path.split('/')[-1].split('.')[0]
-            main(img_path=image_path, save_path=f'noise_{img_name}_{noise_sigma}.pkl', noise_sigma=noise_sigma)
+            if draw_results:
+                draw_res(image_path=image_path, noise_sigma=noise_sigma)
+            else:
+                img_name = image_path.split('/')[-1].split('.')[0]
+                write_res(img_path=image_path, save_path=f'noise_{img_name}_{noise_sigma}.pkl', noise_sigma=noise_sigma)
--- lib/plot_part.py
+++ lib/plot_part.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 import itertools
 
 
+def save_and_show(fig, save_path, show):
+    if type(save_path) == str:
+        print(f'saving to {save_path}')
+        plt.savefig(save_path, dpi=300)
+    if show:
+        fig.show()
+
+
 def draw_image(ax, img_arr, title):
     ax.imshow(img_arr, cmap='gray')
     ax.set_title(title)
@@ -27,20 +35,20 @@ def draw_hist(ax, hist, title, x_lim=None, y_lim=None):
 def draw_images_and_hists(imgs, hists, titles, show=True, save_path=None):
     assert len(imgs) == len(hists)
     fig, axes = plt.subplots(len(hists), 2, figsize=(15, 15))
-    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+    fig.tight_layout()
+
     hist_y_lim = max(itertools.chain.from_iterable(hists))
     for i, (img, hist, title) in enumerate(zip(imgs, hists, titles)):
         draw_image(ax=axes[i, 0], img_arr=img, title=f"{title} image")
         draw_hist(ax=axes[i, 1], hist=hist, y_lim=hist_y_lim, title=f"{title} histogram")
 
-    if type(save_path) == str:
-        plt.savefig(save_path, dpi=300)
-    if show:
-        fig.show()
+    save_and_show(fig=fig, save_path=save_path, show=show)
 
 
 def draw_images(imgs, titles, plt_shape, show=True, save_path=None):
     assert len(imgs) == len(titles)
+    plt.rc('font', size=14)
+
     fig, axes = plt.subplots(*plt_shape, figsize=(15, 15))
     fig.tight_layout()
 
@@ -50,7 +58,27 @@ def draw_images(imgs, titles, plt_shape, show=True, save_path=None):
         else:
             draw_image(ax=axes[i], img_arr=img, title=title)
 
-    if type(save_path) == str:
-        plt.savefig(save_path, dpi=300)
-    if show:
-        fig.show()
+    save_and_show(fig=fig, save_path=save_path, show=show)
+
+
+def draw_losses(ax, losses, legends, title, x_lim=None, y_lim=None):
+    for loss in losses:
+        ax.plot(loss, linewidth=4)
+
+    ax.set_title(title)
+    ax.legend(legends, loc='upper right')
+    if x_lim is not None:
+        ax.set_xlim([None, x_lim])
+    if y_lim is not None:
+        ax.set_ylim(None, y_lim)
+
+
+def draw_image_and_loss(loss_keys, loss_list, title, show=True, save_path=None):
+    plt.rc('font', size=30)
+
+    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+    fig.tight_layout()
+
+    draw_losses(ax=ax, losses=loss_list, legends=loss_keys, title=f"{title} loss history")
+
+    save_and_show(fig=fig, save_path=save_path, show=show)
